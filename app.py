6614a91d616912d4615c5aca2a47ba9be6ac9825
imosudi <imosudi@gmail.com>
2018-06-18 04:19:17 +0100

    App Template Setup

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    #return 'Hello, World!'
+    return  render_template('index.html')
 
 
 
