f910cd35ba1907fb497d0891fa2c3d3b1fcaee4d
imosudi <imosudi@gmail.com>
2018-06-19 18:52:45 +0100

    App Update

--- app.py
+++ app.py
@@ -19,7 +19,16 @@ def register():
 	return render_template('register.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    # note that we set the 500 status explicitly
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
