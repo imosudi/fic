058c79c2ac27bb81ab25a5d8dbc6174e3df8d5e4
imosudi <imosudi@gmail.com>
2018-06-21 18:34:38 +0100

    App Forms and Views

---
+++ views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+@app.route('/registeration', methods=['GET', 'POST'])
+def registeration():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
